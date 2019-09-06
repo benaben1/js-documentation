d93e2d421bb98ef22a084bbc30da4d0e0e323c95
peninah <peninahnamutosi222@gmail.com>
2019-09-06 11:33:28 +0300

    Changes to be committed:

---
+++ challenge/challenge2a.java
@@ -0,0 +1,11 @@
+public class Numbers{
+    public static void main(string args
+    []){
+        int a,c;
+        for(a=2; a<15;a++){
+            c=a*a;
+            system.out.println("The squares of the numbers include,"+c);
+
+        }
+    }
+}
